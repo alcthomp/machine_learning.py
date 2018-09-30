ff90b601638a0864fa47509246e02061c96566bb
Alan Thompson <alcthomp@gmail.com>
2018-09-30 15:41:56 -0500

    uncomment ML

--- machineLearning.py
+++ machineLearning.py
@@ -2,26 +2,26 @@
 import scipy
 import numpy
 import anaconda
-#import sklearn
-# Load libraries
-#import pandas
-##from pandas.plotting import scatter_matrix
-##import matplotlib.pyplot as plt
-##from sklearn import model_selection
-##from sklearn.metrics import classification_report
-##from sklearn.metrics import confusion_matrix
-##from sklearn.metrics import accuracy_score
-##from sklearn.linear_model import LogisticRegression
-##from sklearn.tree import DecisionTreeClassifier
-##from sklearn.neighbors import KNeighborsClassifier
-##from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-##from sklearn.naive_bayes import GaussianNB
-##from sklearn.svm import SVC
+import sklearn
+ #Load libraries
+import pandas
+from pandas.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn import model_selection
 
 # Load dataset
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
-#dataset = pandas.read_csv(url, names=names)
+dataset = pandas.read_csv(url, names=names)
 def importML():
         # Check the versions of libraries
 
@@ -38,42 +38,42 @@ def importML():
         import matplotlib
         print('matplotlib: {}'.format(matplotlib.__version__))
         # pandas 
-        #import pandas 
-        #print('pandas: {}'.format(pandas.__version__))
+        import pandas 
+        print('pandas: {}'.format(pandas.__version__))
         # scikit-learn
         import sklearn
         print('sklearn: {}'.format(sklearn.__version__))
 
 
+
+
+def crap():
+          print("hello")
+        print(dataset.shape)
+        print(dataset.describe())
+        print(dataset.groupby('class').size())
+        dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+        plt.show()
+        dataset.hist()
+        plt.show()
+        # histograms
+        dataset.hist()
+        plt.show()
+        scatter_matrix(dataset)
+        plt.show()
+
+        
+        clf = RandomForestClassifier(n_jobs=4,n_estimators=ops.numtrees, oob_score=True)
+        Y,_=pd.factorize(train['class'])
+        clf.fit(train.drop('class',axis=1),y)
+        test['prediction'] = clf.predict(testnoclass)
+
+	#malware-traffic-analysis.net
+        #contagio malware dump
 def main():
         importML()   
 if __name__ == "__main__":
         main()
-
-def crap():
-          print("hello")
-##        print(dataset.shape)
-##        print(dataset.describe())
-##        print(dataset.groupby('class').size())
-##        dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
-##        plt.show()
-##        dataset.hist()
-##        plt.show()
-##        # histograms
-##        dataset.hist()
-##        plt.show()
-##        scatter_matrix(dataset)
-##        plt.show()
-
-        
-        #clf = RandomForestClassifier(n_jobs=4,n_estimators=ops.numtrees, oob_score=True)
-        #Y,_=pd.factorize(train['class'])
-        #clf.fit(train.drop('class',axis=1),y)
-        #test['prediction'] = clf.predict(testnoclass)
-
-	#malware-traffic-analysis.net
-        #contagio malware dump
-
 
 
 
